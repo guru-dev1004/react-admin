99c28e7b6584f3367fdf7dde5f97051c6d49b2b1
Eric-Lin-Dev <eric.lin.dev@outlook.com>
2020-07-26 10:08:35 -0700

    Feat: AuthProvider Added

--- src/authProvider.js
+++ src/authProvider.js
@@ -1,25 +1,25 @@
 export default {
   //called when the user attemts to login
   login: ({ username }) => {
-    localStorage.setItem("username", username);
+    localStorage.setItem("token", username);
     return Promise.resolve();
   },
   // called when the user clicks on the logout button
   logout: () => {
-    localStorage.removeItem("username");
+    localStorage.removeItem("token");
     return Promise.resolve();
   },
   // called when the API returns an error
   checkError: ({ status }) => {
     if (status === 401 || status === 403) {
-      localStorage.removeItem("username");
+      localStorage.removeItem("token");
       return Promise.reject();
     }
     return Promise.reject();
   },
   // called when the user navigates to a new location, to check for authentication
   checkAuth: () => {
-    return localStorage.getItem("username")
+    return localStorage.getItem("token")
       ? Promise.resolve()
       : Promise.reject();
   },
