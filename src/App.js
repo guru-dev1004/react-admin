d118d06e9ed3293ceffa49c4e589ce20f6c1bbe9
Eric-Lin-Dev <eric.lin.dev@outlook.com>
2020-07-26 04:43:19 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { Admin, Resource } from "react-admin";
+import { PostList, PostEdit, PostCreate } from "./posts";
+import { UserList } from "./users";
+import DashBoard from './Dashboard';
+import authProvider from './authProvider';
+import PostIcon from "@material-ui/icons/Book";
+import UserIcon from "@material-ui/icons/Group";
+import jsonServerProvider from "ra-data-json-server";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+const App = () => (
+  <Admin dataProvider={dataProvider} dashboard={DashBoard} authProvider={authProvider}>
+    <Resource
+      name="posts"
+      list={PostList}
+      edit={PostEdit}
+      create={PostCreate}
+      icon={PostIcon}
+    />
+    <Resource name="users" list={UserList} icon={UserIcon} />
+  </Admin>
+);
 
 export default App;
---
+++ src/posts.js
@@ -0,0 +1,84 @@
+import * as React from "react";
+import { useMediaQuery } from "@material-ui/core";
+import {
+  List,
+  Datagrid,
+  TextField,
+  ReferenceField,
+  EditButton,
+  Edit,
+  Create,
+  SimpleForm,
+  ReferenceInput,
+  SelectInput,
+  TextInput,
+  Filter,
+  SimpleList,
+} from "react-admin";
+
+const PostFiler = (props) => (
+  <Filter {...props}>
+    <TextInput label="Search" source="q" alwaysOn />
+    <ReferenceInput label="User" source="userId" reference="users" allowEmpty>
+      <SelectInput optionText="name" />
+    </ReferenceInput>
+  </Filter>
+);
+
+export const PostList = (props) => {
+  const isSmall = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+  return (
+    <List filters={<PostFiler />} {...props}>
+      {isSmall ? (
+        <SimpleList
+          primaryText={(record) => record.title}
+          secondaryText={(record) => `${record.views} views`}
+          tertiaryText={(record) =>
+            new Date(record.published_at).toLocaleDateString()
+          }
+        />
+      ) : (
+        <Datagrid>
+          <TextField source="id" />
+          <ReferenceField source="userId" reference="users">
+            <TextField source="name" />
+          </ReferenceField>
+          <TextField source="id" />
+          <TextField source="title" />
+          <EditButton />
+        </Datagrid>
+      )}
+    </List>
+  );
+};
+const PostTitle = ({ record }) => {
+  return <span>Post {record ? `"${record.title}"` : ""}</span>;
+};
+
+export const PostEdit = (props) => (
+  <Edit title={<PostTitle />} {...props}>
+    <SimpleForm>
+      <TextInput source="id" disabled />
+      <ReferenceInput source="userId" reference="users">
+        <SelectInput optionText="name" />
+      </ReferenceInput>
+
+      <TextInput source="title" />
+      <TextInput multiline source="body" />
+    </SimpleForm>
+  </Edit>
+);
+
+export const PostCreate = (props) => (
+  <Create {...props}>
+    <SimpleForm>
+      <TextInput source="id" disabled />
+      <ReferenceInput source="userId" reference="users">
+        <SelectInput optionText="name" />
+      </ReferenceInput>
+
+      <TextInput source="title" />
+      <TextInput multiline source="body" />
+    </SimpleForm>
+  </Create>
+);
